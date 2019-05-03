55dd9984179dd67e8ec2e1978f0e97b9698b8c91
giopogg <49613661+giopogg@users.noreply.github.com>
2019-05-03 15:23:14 +0200

    Update conv_test.R

--- simcoms-master/conv_test.R
+++ simcoms-master/conv_test.R
@@ -14,30 +14,48 @@ formula<- ~env + env2
 mod_gjam_low <- gjam(formula, xdata, ydata, modelList = ml)
 ## to set thininhg - choose by =..
 ind<-seq(1,dim(mod_gjam_low$chains$sgibbs)[1], by=10)
-gjam_mc<- mcmc(mod_gjam_low$chains$sgibbs[ind,])
-s2s1<- mcmc(mod_gjam_low$chains$sgibbs[ind,2])
-acfplot(gjam_mc)  ##autocor plot
-ggs_autocorrelation(ggs(gjam_mc)) ###autocr
 
-#only non-diagonal
+
+gjam_mc<- mcmc(mod_gjam_low$chains$sgibbs[ind,]) #thinned sigma
+s2s1<- mcmc(mod_gjam_low$chains$sgibbs[ind,2]) #s2s1 chain
 ind_d<-vector()
 ind_d[1]<-1
 for (i in 1:9) ind_d[i+1]<- ind_d[i]+ns-i+1
-gjam_mc_nd<- mcmc(mod_gjam_low$chains$sgibbs[ind,-ind_d])
+gjam_mc_nd<- mcmc(mod_gjam_low$chains$sgibbs[ind,-ind_d]) #thinned sigma non diagonal elements
 
+#acf
+#acfplot(gjam_mc)  ##autocor plot
+ggs_autocorrelation(ggs(gjam_mc)) ###autocr
 
-plotESSBurn(gjam_mc)  ### describe how much should be done the burnin
+#traceplots
+x11()
+plot(gjam_mc) #traceplots
 
+#nESS crazy low
 xyplot(s2s1)
-
 hist(effectiveSize(gjam_mc), main="ess(sigma)",lwd=2,col=gray(.6),breaks=100)
-hist(effectiveSize(gjam_mc_nd), main="ess(sigma)",lwd=2,col=gray(.6))
+hist(effectiveSize(gjam_mc_nd), main="ess(sigma) only non diagonal terms",lwd=2,col=gray(.6))
 
-hist(effectiveSize(mod_gjam1$chains$bgibbs), main="ess(beta)",lwd=2,col=gray(.6))
-# 
-#JSDM
+#cumuplot
+cumuplot(s2s1)
+x11()
+cumuplot(gjam_mc)
+dev.off()
 
-# me_mcmc<- mcmc(me5$sims.list$Rho[,4,1])
-# xyplot(me_mcmc)
-# 
+#plotESSBurn(gjam_mc)  ### describe how much should be done the burnin
+
+
+# BETA
+
+beta_mcmc<-mcmc(mod_gjam_low$chains$bgibbs)
+ggs_autocorrelation(ggs(beta_mcmc)) ###autocr
+
+hist(effectiveSize(beta_mcmc), main="ess(beta)",lwd=2,col=gray(.6))
+x11()
+plot(beta_mcmc)
+dev.off()
+
+x11()
+cumuplot(beta_mcmc)
+dev.off()
 
