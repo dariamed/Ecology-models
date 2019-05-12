edd3ab764274b250d726d6b2f6539ddc4e0eb05e
bystrova <bystrova.daria@gmail.com>
2019-05-12 22:35:24 +0200

    conv_all
    All convergence in the loading_all_models

--- simcoms-master/loading_all_models.R
+++ simcoms-master/loading_all_models.R
@@ -211,6 +211,35 @@ p2<- ggplot(hmsc_conv_dataset, aes(x=value, color=parameter,fill=parameter)) +
 p2
 dev.off()
 
+###########################################GJAM functions#######################################################################
+
+
+gj_conv<-function(name){
+  gj_mod<-load_object(name)
+  gjam_bs<- mcmc.list(mcmc(gj_mod$m1$chains$bgibbsUn[-(1:burn),]),mcmc(gj_mod$m2$chains$bgibbsUn[-(1:burn),]))
+  gjam_sigma<- mcmc.list(mcmc(gj_mod$m1$chains$sgibbs[-(1:burn),]),mcmc(gj_mod$m2$chains$sgibbs[-(1:burn),]))
+  ###NEW plot for effective size
+  n_eff_beta<- as.data.frame(effectiveSize(gjam_bs))
+  colnames(n_eff_beta)<- c("value")
+  n_eff_beta$parameter<- "beta"
+  n_eff_sigma<- as.data.frame(effectiveSize(gjam_sigma))
+  colnames(n_eff_sigma)<- c("value")
+  n_eff_sigma$parameter<- "correlation"
+  neff<- rbind(n_eff_beta,n_eff_sigma)
+  
+  
+  Rhat_beta<-as.data.frame(gelman.diag(gjam_bs, multivariate=FALSE)$psrf)
+  Rhat_beta$parameter<- "beta"
+  Rhat_sigma<- as.data.frame(gelman.diag(gjam_sigma, multivariate=FALSE)$psrf)
+  Rhat_sigma$parameter<- "correlation"
+  Rhat<- rbind(Rhat_beta,Rhat_sigma)
+  
+  return(list(neff=neff,Rhat=Rhat ))
+}
+
+
+
+###########################################GJAM functions #######################################################################
 
 
 
@@ -225,10 +254,90 @@ gjam_files_list<-list(env5="./gjam_models/gjam5env.rda",env10="./gjam_models/gja
 
 
 
+gjam_list<- lapply(gjam_files_list,gj_conv)
+gjam_conv_dataset<- data.frame()
+
+for(j in 1:length(gjam_list)){
+  tmp1<- as.data.frame(gjam_list[[j]][1])
+  colnames(tmp1)<-c("value","parameter")
+  tmp1$type<-"Effective Size"
+  tmp1$Filtering<-sim_names[[j]]
+  tmp2<- as.data.frame(gjam_list[[j]][2])
+  colnames(tmp2)<-c("value", "value2","parameter")
+  tmp2<-tmp2[,-2]
+  tmp2$type<-"Rhat"
+  tmp2$Filtering<-sim_names[[j]]
+  gjam_conv_dataset<-rbind(gjam_conv_dataset,tmp1,tmp2)  
+}
+
+for(i in 1:dim(gjam_conv_dataset)[1]){
+  if(length(grep("Fac",gjam_conv_dataset$Filtering[i]))>0){ gjam_conv_dataset$gentype[i]<-"Facilitation"}
+  if(length(grep("Comp",gjam_conv_dataset$Filtering[i]))>0){ gjam_conv_dataset$gentype[i]<-"Competition"}
+  if(length(grep("Env",gjam_conv_dataset$Filtering[i]))>0){ gjam_conv_dataset$gentype[i]<-"Environmental"}
+  if((length(grep("Fac",gjam_conv_dataset$Filtering[i]))>0)&(length(grep("Comp",gjam_conv_dataset$Filtering[i]))>0)){
+    gjam_conv_dataset$gentype[i]<-"Comp+Fac"
+  }
+}
+
+
+pdf("plot_conv_gjam.pdf")
+p2<- ggplot(gjam_conv_dataset, aes(x=value, color=parameter,fill=parameter)) +
+  geom_histogram( alpha=0.4, position="identity") +
+  scale_color_brewer(palette="Dark2")+scale_fill_brewer(palette="Dark2") +facet_wrap(type~.,scales = "free_x") +xlab(" ")+
+  ggtitle("Convergence parameters for the GJAM model")+theme_bw()+ theme(plot.title = element_text(hjust = 0.5))
+p2
+dev.off()
+
+
+#########GJAM DIMENSION REDUCTION########################################################################
 
 
 
 
-gjam_dr_files_list<-list(env5=,env10=,env20=,facd5=,facd10=,facd20=, facs5=,facs10=,facs20=,
-                      compd5=,compd10=,compd20=, comps5=,comps10=,comps20=,compfacd5=,compfacd10=,compfacd20=, compfacs5=,compfacs10=,compfacs20=)
+#########GJAM DIMENSION REDUCTION########################################################################
+gjam_dr_files_list<-list(env5="./gjam_models/gjamDR5env.rda",env10="./gjam_models/gjamDR10env.rda",env20="./gjam_models/gjamDR20env.rda",
+                         facd5="./gjam_models/gjamDR5facd.rda",facd10="./gjam_models/gjamDR10facd.rda",facd20="./gjam_models/gjamDR20facd.rda",
+                         facs5="./gjam_models/gjamDR5facs.rda",facs10="./gjam_models/gjamDR10facs.rda",facs20="./gjam_models/gjamDR20facs.rda",
+                      compd5="./gjam_models/gjamDR5compd.rda",compd10="./gjam_models/gjamDR10compd.rda",compd20="./gjam_models/gjamDR20compd.rda",
+                      comps5="./gjam_models/gjamDR5comps.rda",comps10="./gjam_models/gjamDR10comps.rda",comps20="./gjam_models/gjamDR20comps.rda",
+                      compfacd5="./gjam_models/gjamDR5compfacd.rda",compfacd10="./gjam_models/gjamDR10compfacd.rda",compfacd20="./gjam_models/gjamDR20compfacd.rda",
+                      compfacs5="./gjam_models/gjamDR5compfacs.rda",compfacs10="./gjam_models/gjamDR10compfacs.rda",compfacs20="./gjam_models/gjamDR20compfacs.rda")
+
+
+
+gjam_dr_list<- lapply(gjam_dr_files_list,gj_conv)
+gjam_dr_conv_dataset<- data.frame()
+
+for(j in 1:length(gjam_dr_list)){
+  tmp1<- as.data.frame(gjam_dr_list[[j]][1])
+  colnames(tmp1)<-c("value","parameter")
+  tmp1$type<-"Effective Size"
+  tmp1$Filtering<-sim_names[[j]]
+  tmp2<- as.data.frame(gjam_dr_list[[j]][2])
+  colnames(tmp2)<-c("value", "value2","parameter")
+  tmp2<-tmp2[,-2]
+  tmp2$type<-"Rhat"
+  tmp2$Filtering<-sim_names[[j]]
+  gjam_dr_conv_dataset<-rbind(gjam_dr_conv_dataset,tmp1,tmp2)  
+}
+
+for(i in 1:dim(gjam_dr_conv_dataset)[1]){
+  if(length(grep("Fac",gjam_dr_conv_dataset$Filtering[i]))>0){ gjam_dr_conv_dataset$gentype[i]<-"Facilitation"}
+  if(length(grep("Comp",gjam_dr_conv_dataset$Filtering[i]))>0){ gjam_dr_conv_dataset$gentype[i]<-"Competition"}
+  if(length(grep("Env",gjam_dr_conv_dataset$Filtering[i]))>0){ gjam_dr_conv_dataset$gentype[i]<-"Environmental"}
+  if((length(grep("Fac",gjam_dr_conv_dataset$Filtering[i]))>0)&(length(grep("Comp",gjam_dr_conv_dataset$Filtering[i]))>0)){
+    gjam_dr_conv_dataset$gentype[i]<-"Comp+Fac"
+  }
+}
+
+pdf("plot_conv_gjamdr.pdf")
+p2<- ggplot(gjam_dr_conv_dataset, aes(x=value, color=parameter,fill=parameter)) +
+  geom_histogram( alpha=0.4, position="identity") +
+  scale_color_brewer(palette="Dark2")+scale_fill_brewer(palette="Dark2") +facet_wrap(type~.,scales = "free_x") +xlab(" ")+
+  ggtitle("Convergence parameters for the DR_GJAM model")+theme_bw()+ theme(plot.title = element_text(hjust = 0.5))
+p2
+dev.off()
+
+
+
 
