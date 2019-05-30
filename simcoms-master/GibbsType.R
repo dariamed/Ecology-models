2484d4e500785d5b06870f2e020c585975fb5a9f
bystrova <bystrova.daria@gmail.com>
2019-05-30 14:56:02 +0200

    Update GibbsType.R

--- simcoms-master/GibbsType.R
+++ simcoms-master/GibbsType.R
@@ -44,7 +44,8 @@ v_ng<- function(beta, sigma, kval, npoints){
   coef_high<-exp(beta)* sigma^(kval-1)
   coef<- coef_high/coef_low
   for(i in (0:(npoints-1))){
-    gn<- incgam(beta,kval - i/sigma)
+    gn<- gammainc(kval - i/sigma,beta)[2]
+    #gn<- gamma_inc(kval - i/sigma,beta)
     ckn<- choose(npoints-1,i)
     sum<- sum + ((-1)^i)*(beta^(i/sigma))*ckn*gn
   }
@@ -105,13 +106,13 @@ prob<- as.numeric(prob_brob)
 #############################Plotting###############################################################################
 k_vec<-seq(1,50,by=49/9)
 sigma_vec<-seq(0.2,0.8, by=0.6/9)
-z<- outer(k_vec,sigma_vec,Vectorize(prob_py),npoints=50, theta=1)
+z<- outer(k_vec,sigma_vec,Vectorize(prob_ng),npoints=50, beta=1)
 
-# p<- plot_ly(showscale = TRUE) %>%
-#   add_surface(x=k_vec, y=sigma_vec,z =z, cmin = min(z), cmax = max(z),colorbar=list(title='PY'), colorscale = list(c(0,1),c("rgb(255,112,184)","rgb(128,0,64)")),opacity = 0.98) %>%
-#   layout(title="Prior distribution", scene = list(xaxis= list(title="K"),yaxis= list(title="sigma"),zaxis= list(title="N",range = c(min(z),max(z)))))
-# p
-# 
+p<- plot_ly(showscale = TRUE) %>%
+   add_surface(x=k_vec, y=sigma_vec,z =z, cmin = min(z), cmax = max(z),colorbar=list(title='PY'), colorscale = list(c(0,1),c("rgb(255,112,184)","rgb(128,0,64)")),opacity = 0.98) %>%
+   layout(title="Prior distribution", scene = list(xaxis= list(title="K"),yaxis= list(title="sigma"),zaxis= list(title="N",range = c(min(z),max(z)))))
+p
+#
 # k_vec<- seq(1,50, by =1)
 # y<- c()
 # for(l in (1: length(k_vec))){
@@ -165,7 +166,7 @@ p
 #prob_dir<- function(k, npoints, theta)
 
 
-theta_df <- rep(seq(1,100, by=99/9), each=150)
+theta_df <- rep(seq(1,150, by=149/9), each=150)
 k_df<- rep(1:150, 10)
 df<- as.data.frame(matrix(NA, nrow=1500, ncol=1))
 
@@ -184,7 +185,44 @@ p150
 
 
 
-#prob_dir(10,npoints=150,theta=60)
+prob_dir(10,npoints=150,theta=60)
+
+
+#####################################################################
+
+
+
+
+theta_df <- rep(seq(1,150, by=149/9), each=150)
+k_df<- rep(1:150, 10)
+df<- as.data.frame(matrix(NA, nrow=1500, ncol=1))
+
+df$k<- k_df
+df$theta<- theta_df
+for(l in (1: nrow(df))){
+  df$val[l]<- prob_dir_large_dim(df$k[l],npoints=150,theta=df$theta[l])
+}
+
+df$theta<- as.factor(df$theta)
+p150<- plot_ly(df, x =df$k , y = df$theta, z = df$val, split = df$theta, type = "scatter3d", mode = "lines") %>% 
+  layout(title="Prior density for Dirichlet prior for n=50", scene = list(xaxis= list(title="K"),yaxis= list(title="alpha"), zaxis= list(title="Probability")))                                                                                          
+
+p150
+#theta_df <- rep(seq(1,150, by=149/9), each=150)
+k_df<- rep(1:50,  by=49/9)
+npoint_df <- rep(seq(50,140, by=10), each=50)
+
+df<- as.data.frame(matrix(NA, nrow=500, ncol=1))
+
+df$k<- k_df
+df$np<- npoint_df
+for(l in (1: nrow(df))){
+  df$val[l]<- prob_dir_large_dim(df$k[l],npoints=df$np[l],theta=20)
+}
+df$np<- as.factor(df$np)
+p150<- plot_ly(df, x =df$k , y = df$np, z = df$val, split = df$np, type = "scatter3d", mode = "lines") %>% 
+  layout(title="Prior density for Dirichlet prior theta=20, varying n in (50-140) ", scene = list(xaxis= list(title="K"),yaxis= list(title="number of points"), zaxis= list(title="Probability")))
+p150
 
 
 ###################
@@ -201,21 +239,38 @@ p150
 #   }
 # }
 
+# 
+# k<-60
+# npoints<-150
+# n_vec<-as.brob( 0:(npoints-1))
+# theta_n<- prod(theta +n_vec)
+# stir<- as.brob(abs(Stirling1(npoints,k)))
+# powerk<- as.brob((theta^k))
+# prob_brob<- powerk*(stir/theta_n)
+# prob<- as.numeric(prob_brob)
+# 
 
-k<-60
-npoints<-150
-n_vec<-as.brob( 0:(npoints-1))
-theta_n<- prod(theta +n_vec)
-stir<- as.brob(abs(Stirling1(npoints,k)))
-powerk<- as.brob((theta^k))
-prob_brob<- powerk*(stir/theta_n)
-prob<- as.numeric(prob_brob)
+#theta_df <- rep(seq(1,150, by=149/9), each=150)
+k_df<- rep(1:50,  by=49/9)
+npoint_df <- rep(seq(50,140, by=10), each=50)
+
+df<- as.data.frame(matrix(NA, nrow=500, ncol=1))
+
+df$k<- k_df
+df$np<- npoint_df
+for(l in (1: nrow(df))){
+  df$val[l]<- prob_dir_large_dim(df$k[l],npoints=df$np[l],theta=20)
+}
+df$np<- as.factor(df$np)
+p150<- plot_ly(df, x =df$k , y = df$np, z = df$val, split = df$np, type = "scatter3d", mode = "lines") %>% 
+  layout(title="Prior density for Dirichlet prior theta=20, varying n in (50-140) ", scene = list(xaxis= list(title="K"),yaxis= list(title="number of points"), zaxis= list(title="Probability")))
+p150
 
 
-x <- as.brob(1:10)
-y <- 1e10
-x+y
-as.numeric((x+y)-1e10)
-x^(1/y)
+
+
+
+
+
 
 
